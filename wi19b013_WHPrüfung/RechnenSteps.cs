7a38dcb91f6e43278a5cc33147b0ba7ac86c891e
Georg <georg.pretzner@gmail.com>
2020-12-02 16:25:20 +0100

    Add project files.

---
+++ wi19b013_WHPrüfung/RechnenSteps.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace wi19b013_WHPrüfung
+{
+    [Binding]
+    public class RechnenSteps
+    {
+        int number, number2, result;
+        [Given(@"the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int p0)
+        {
+            number = p0;
+        }
+        
+        [Given(@"the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int p0)
+        {
+            number2 = p0;
+        }
+        
+        [When(@"the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            result = number + number2;
+        }
+        
+        [When(@"the second number is substracted from the first number")]
+        public void WhenTheSecondNumberIsSubstractedFromTheFirstNumber()
+        {
+            result = number - number2;
+        }
+        
+        [When(@"the two numbers are multiplicated")]
+        public void WhenTheTwoNumbersAreMultiplicated()
+        {
+            result = number * number2;
+        }
+        
+        [When(@"the first number is divided by the second number")]
+        public void WhenTheFirstNumberIsDividedByTheSecondNumber()
+        {
+            result = number / number2;
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            int expected = p0;
+            int actual = result;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ wi19b013_WHPrüfung/SpecFlowFeature1.feature
@@ -0,0 +1,30 @@
+﻿Feature: Rechnen
+	Um Rechenoperationen 
+	als ein laie korrekt durchzuführen
+	will ich die Grundrechenarten durchführen
+
+@mytag
+Scenario: Addition
+	Given the first number is 4
+	And the second number is 2
+	When the two numbers are added
+	Then the result should be 6
+
+
+Scenario: Subtraktion
+	Given the first number is 4
+	And the second number is 2
+	When the second number is substracted from the first number
+	Then the result should be 2
+
+Scenario: Multiplikation
+	Given the first number is 4
+	And the second number is 2
+	When the two numbers are multiplicated
+	Then the result should be 8
+
+Scenario: Division
+	Given the first number is 4
+	And the second number is 2
+	When the first number is divided by the second number
+	Then the result should be 2
